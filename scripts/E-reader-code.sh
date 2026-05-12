a5c73205aa605c62119ed1b2d64ef003a2ee1742
bagi <alisson.alves1628@gmail.com>
2026-05-11 23:22:31 -0300

    feat: instala as depêndencias da automação

--- scripts/E-reader-code.sh
+++ scripts/E-reader-code.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
+# Define as variavéis do código
+REPO_LOCAL=$(/home/$USER/android_e-reader_project)
+PACKAGES=$(android-tools curl bc)
 
-# 1. Verifica se há exatamente um dispositivo conectado e autorizado
+# Instalando dependências 
+install_dependencies() {
+	if [ -x "$(command -v apt-get)"]; then 
+		sudo apt-get update && sudo apt-get install -y $PACKAGES
+	elif [ -x "$(command -v dnf)"]; then
+		sudo dnf install -y $PACKAGES
+	elif [-x "$(command -v pacman)"]; then
+		sudo pacman -Sy --no-confirm $PACKAGES
+	elif [ -x "$(command -v zypper)" ]; then
+        	sudo zypper install -y $PACKAGES
+	else 
+		echo "ERRO: Falha ao identificar gerenciador de pacotes. verifique as permissões ou instale manualmente"
+		exit 1 
+	fi
+}
+
+# Verifica se há exatamente um dispositivo conectado e autorizado
 # O comando 'adb devices' lista os IDs. Filtramos a linha do cabeçalho e linhas vazias.
 DEVICE_CHECK=$(adb devices | grep -v "List of devices attached" | grep "device$" | wc -l)
 
 if [ "$DEVICE_CHECK" -eq 1 ]; then
     echo "Status: Dispositivo compatível encontrado."
 
-    # 2. Extrai o modelo do dispositivo usando getprop
+    # Extrai o modelo do dispositivo usando getprop
     # O comando limpa caracteres de escape (\r) comuns em saídas do Android
     MODELO_DISPOSITIVO=$(adb shell getprop ro.product.model | tr -d '\r')
 
