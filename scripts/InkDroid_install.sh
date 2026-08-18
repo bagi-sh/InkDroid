1b5578900858413787b38c416ce3638324932acf
bagi <alisson.alves1628@gmail.com>
2026-08-18 00:26:28 -0300

    feat: add installation of Olauncher and Koreader

--- scripts/InkDroid_install.sh
+++ scripts/InkDroid_install.sh
@@ -2,10 +2,10 @@
 # Define as variavéis do código
 REPO="/home/$USER/InkDroid"
 PACKAGES="android-tools curl bc jq"
-LOG_REMOVACOES="$REPO/debloatlog.txt"
-touch "$LOG_REMOVACOES"
+LOG_ACTIONS="$REPO/debloatlog.txt"
+touch "$LOG_ACTIONS"
 JSON="$REPO/dependences/blacklist.json"
-# Instalando dependências 
+
 install_dependencies() {
 	if [ -x "$(command -v apt-get)" ]; then 
 		sudo apt-get update && sudo apt-get install -y $PACKAGES
@@ -14,27 +14,24 @@ install_dependencies() {
 	elif [ -x "$(command -v pacman)" ]; then
 		sudo pacman -Sy --noconfirm $PACKAGES
 	elif [ -x "$(command -v zypper)" ]; then
-       	sudo zypper install -y $PACKAGES
+    sudo zypper install -y $PACKAGES
 	else 
 		echo "ERRO: Falha ao identificar gerenciador de pacotes. verifique as permissões ou instale manualmente"
 		exit 1 
 	fi				 	 
 }
-install_dependencies
+
+install_dependencies()
 # Verifica se há exatamente um dispositivo conectado e autorizado
-# O comando 'adb devices' lista os IDs. Filtramos a linha do cabeçalho e linhas vazias.
 DEVICE_CHECK=$(adb devices | grep -v "List of devices attached" | grep "device$" | wc -l)
 
 if [ "$DEVICE_CHECK" -eq 1 ]; then
     echo "Status: Dispositivo compatível encontrado."
 
-    # Extrai o modelo do dispositivo usando getprop
-    # O comando limpa caracteres de escape (\r) comuns em saídas do Android
-    MODELO=$(adb shell getprop ro.product.model | tr -d '\r')
+    MODEL=$(adb shell getprop ro.product.model | tr -d '\r')
 
     echo "Dispositivo identificado"
     
-    # Exemplo de uso da variável
     echo "Iniciando procedimentos para o dispositivo"
 
 elif [ "$DEVICE_CHECK" -gt 1 ]; then
@@ -47,12 +44,12 @@ else
 fi
 
 ANDROID_VER=$(adb shell getprop ro.build.version.release)
-FABRICANTE=$(adb shell getprop ro.product.manufacturer)
+VENDOR=$(adb shell getprop ro.product.manufacturer)
 
 echo -e "\e[32m[CONECTADO]\e[0m Dispositivo detectado com sucesso!"
 echo "--------------------------------------------------"
-echo " Fabricante: $FABRICANTE"
-echo " Modelo:     $MODELO"
+echo " Fabricante: $VENDOR"
+echo " Modelo:     $MODEL"
 echo " Android:    $ANDROID_VER"
 echo "--------------------------------------------------"
 
@@ -63,10 +60,10 @@ if [ ! -f "$JSON" ]; then
 fi
 
 # Inicializa o arquivo de log/relatório
-echo "=== RELATÓRIO DE DEBLOAT ===" > "$LOG_REMOVACOES"
-echo "Apareilho: $FABRICANTE $MODELO (Android $ANDROID_VER)" >> "$LOG_REMOVACOES"
-echo "Data da execução: $(date)" >> "$LOG_REMOVACOES"
-echo "---------------------------------" >> "$LOG_REMOVACOES"
+echo "=== RELATÓRIO DE DEBLOAT ===" > "$LOG_ACTIONS"
+echo "Aparelho: $VENDOR $MODEL (Android $ANDROID_VER)" >> "$LOG_ACTIONS"
+echo "Data da execução: $(date)" >> "$LOG_ACTIONS"
+echo "---------------------------------" >> "$LOG_ACTIONS"
 
 # --- Execução do Processo de Otimização (Parsing do JSON) ---
 
@@ -80,25 +77,51 @@ jq -r '.[] | .[]' "$JSON" | while read -r pacote; do
     
     echo -n "Processando: $pacote ... "
 
- # Executa a desinstalação a nível de usuário comum (User 0) sem necessidade de Root
-# Captura a saída de erro padrão para evitar mensagens poluídas no terminal    
-# usa '< /dev/null' para impedir o ADB de quebrar o fluxo do JQ
-    RESULTADO=$(adb shell pm uninstall -k --user 0 "$pacote" < /dev/null 2>&1)
+    RESULT=$(adb shell pm uninstall -k --user 0 "$pacote" < /dev/null 2>&1)
 
     # Valida o resultado do comando enviado ao Android
-    if echo "$RESULTADO" | grep -q "Success"; then
-        echo -e "\e[32m[REMOVIDO]\e[0m"
-        echo "[SUCESSO] Pacote removido: $pacote" >> "$LOG_REMOVACOES"
-    elif echo "$RESULTADO" | grep -q "not installed"; then
-        echo -e "\e[33m[NÃO ENCONTRADO]\e[0m"
-        echo "[INFO] Pacote ausente na ROM padrão: $pacote" >> "$LOG_REMOVACOES"
+    if echo "$RESULT" | grep -q "Success"; then
+        echo -e "\e[32m[REMOVED]\e[0m"
+        echo "[SUCESS] Unistalled: $pacote" >> "$LOG_ACTIONS"
+    elif echo "$RESULT" | grep -q "not installed"; then
+        echo -e "\e[33m[PACKAGE NOT FOUND]\e[0m"
+        echo "[INFO] Dont find package: $pacote" >> "$LOG_ACTIONS"
     else
-        echo -e "\e[31m[FALHA]\e[0m ($RESULTADO)"
-        echo "[FALHA] Erro ao remover $pacote: $RESULTADO" >> "$LOG_REMOVACOES"
+        echo -e "\e[31m[ERROR]\e[0m ($RESULT)"
+        echo "[FAIL] Error on trying $pacote: $RESULT" >> "$LOG_ACTIONS"
     fi
 
 done
 
 echo "--------------------------------------------------"
 echo -e "\e[32m[CONCLUÍDO]\e[0m Otimização finalizada!"
-echo "O relatório detalhado foi salvo em: $LOG_REMOVACOES"
+echo "O relatório detalhado foi salvo em: $LOG_ACTIONS"
+
+echo "Starting Download for apps"
+
+curl -fL "$(curl fsSL https://api.github.com/repos/koreader/koreader/releases/latest | jq -r '.assets[] | select(.name | endswith(".apk")) | .browser_download_url' | head -n 1)"  -o ./Koreader.apk
+if [ -e ./Koreader.apk ]; then
+  echo "[SUCESS] Downloaded Koreader" && echo "[SUCESS] Downloaded Koreader" >> "$LOG_ACTIONS"
+  echo "Installing..."
+  RESULT=$(adb install -r --user 0 ./Koreader.apk < /dev/null 2>&1)
+  if echo "$RESULT" | grep -q "Success"; then
+    echo "[SUCESS] Koreader Installed" &&  echo "[SUCESS] Koreader Installed" >> "$LOG_ACTIONS"
+  else
+    echo "[ERROR] Something went wrong on Koreader Installation" >> "$LOG_ACTIONS"
+    echo "Error. Process will continue withot Koreader"
+    fi
+fi
+
+curl -fL "$(curl -fsSL https://api.github.com/repos/tanujnotes/Olauncher/releases/latest | jq -r '.assets[] | select(.name | endswith(".apk")) | .browser_download_url' | head -n 1)" -o olauncher.apk
+if [ -e ./olauncher.apk ]; then
+  echo "[SUCESS] Downloaded Olauncher" && echo "[SUCESS] Downloaded Olauncher" >> "$LOG_ACTIONS"
+  echo "Installing..."
+  RESULT=$(adb install -r --user 0 ./olauncher.apk < /dev/null 2>&1)
+  if echo "$RESULT" | grep -q "Success"; then
+    echo "[SUCESS] Koreader Installed" &&  echo "[SUCESS] Koreader Installed" >> "$LOG_ACTIONS"
+  else
+    echo "[ERROR] Something went wrong on Olauncher Installation" >> "$LOG_ACTIONS"
+    echo "Error. Process will continue withot Olauncher"
+    fi
+fi
+
